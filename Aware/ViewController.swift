6dde8d615b96b28e0d88e541f9e3b0a9895e6b40
Pardeep <pardeep.chaudhary@kelltontech.com>
2016-11-03 12:30:11 +0530

    Initial files for spread awareness about society

---
+++ Aware/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Aware
+//
+//  Created by Pardeep Chaudhary on 03/11/16.
+//  Copyright © 2016 Pardeep Chaudhary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func btnSendAction(_ sender: UIBarButtonItem) {
+        
+    }
+}
+
