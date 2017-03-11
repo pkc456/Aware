d39b458a835541257b6a96487bc7a7eb44ab63a8
Pardeep Chaudhary <pkc456@gmail.com>
2017-03-12 00:52:37 +0530

    non-consumable in app integrated

---
+++ Aware/Utility/Constant.swift
@@ -0,0 +1,17 @@
+//
+//  Constant.swift
+//  Aware
+//
+//  Created by Pradeep Choudhary on 3/11/17.
+//  Copyright © 2017 Pardeep Chaudhary. All rights reserved.
+//
+
+import Foundation
+
+//IN_App_Purchase Constants
+let DONATE_PRODUCT_ID = "com.programmingcrew.awaresharedonate"
+let KKEY_PURCHASE_MADE = "nonConsumablePurchaseMade"
+let KDonateButtonTag = 10   //When user has option to donate
+let KRestoreButtonTag = 20  ////When user has option to restore
+
+let KALERT_TITLE = "Aware & Share"
--- Aware/ViewController.swift
+++ Aware/ViewController.swift
@@ -10,18 +10,29 @@ import UIKit
 import MessageUI
 import CoreLocation
 import RMessage
+import StoreKit
 
-class ViewController: UIViewController,MFMailComposeViewControllerDelegate,DSDLocationHandlerDelegate {
+class ViewController: UIViewController,MFMailComposeViewControllerDelegate,DSDLocationHandlerDelegate,SKProductsRequestDelegate,
+SKPaymentTransactionObserver {
 
     @IBOutlet weak var textfieldName: UITextField!
     @IBOutlet weak var textfieldLocation: UITextField!
     @IBOutlet weak var textfieldTitle: UITextField!
     @IBOutlet weak var textViewDetails: UITextView!
     
+    @IBOutlet weak var btnDonate: UIBarButtonItem!
+    var productID = ""
+    var productsRequest = SKProductsRequest()   //productsRequest is an instance of SKProductsRequest, needed to search for IAP products from your app on iTC.
+    var iapProducts = [SKProduct]() //iapProducts is a simple array of SKProducts.
+    var nonConsumablePurchaseMade = UserDefaults.standard.bool(forKey: KKEY_PURCHASE_MADE)  //needed to track purchases
+//    var coins = UserDefaults.standard.integer(forKey: "coins")
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         setUI()
+        startInAppPurchase()
     }
     
     private func setUI(){
@@ -54,9 +65,24 @@ class ViewController: UIViewController,MFMailComposeViewControllerDelegate,DSDLo
         }
     }
     
+    @IBAction func btnDonateAction(_ sender: UIBarButtonItem)
+    {
+        if(sender.tag == KRestoreButtonTag){
+            SKPaymentQueue.default().add(self)
+            SKPaymentQueue.default().restoreCompletedTransactions()
+        }else{
+            if(iapProducts.count>0){
+                purchaseMyProduct(product: iapProducts[0])
+            }else{
+                Utility.showAlertMessage(title: "Server error", subTitle: "Please restart application", messageType: .error)
+            }
+            
+        }
+    }
+    
     //MARL: User defined action
     func showSendMailErrorAlert() {
-        Utility.showAlertMessage(title: "Could Not Send Email", subTitle: "Your device could not send e-mail.  Please check e-mail configuration and try again", messageType: .error)
+        Utility.showAlertMessage(title: "Could Not Send Email", subTitle: "Your device could not send e-mail. Please check e-mail configuration and try again", messageType: .error)
     }
     
     func configuredMailComposeViewController() -> MFMailComposeViewController {
@@ -141,6 +167,140 @@ class ViewController: UIViewController,MFMailComposeViewControllerDelegate,DSDLo
         textfieldLocation.text = values
         
         DSDLocationHandler.sharedInstance.stopLocationUpdate()
+    }
+    
+    //MARK: In App purchase
+    // MARK: - MAKE PURCHASE OF A PRODUCT
+    func canMakePurchases() -> Bool {
+        return SKPaymentQueue.canMakePayments()
+    }
+    
+    //It starts the payment queue and changes our productID variable into the selected productIdentifier
+    func purchaseMyProduct(product: SKProduct) {
+        if self.canMakePurchases() {
+            let payment = SKPayment(product: product)
+            SKPaymentQueue.default().add(self)
+            SKPaymentQueue.default().add(payment)
+            
+            print("PRODUCT TO PURCHASE: \(product.productIdentifier)")
+            productID = product.productIdentifier
+            
+            
+            // IAP Purchases dsabled on the Device
+        } else {
+            UIAlertView(title: KALERT_TITLE,
+                        message: "Purchases are disabled in your device!",
+                        delegate: nil, cancelButtonTitle: "OK").show()
+        }
+    }
+    
+    private func startInAppPurchase(){
+        if nonConsumablePurchaseMade
+        {
+            self.setDataAfterInAppPurchaseTransaction(isPurchased: true)            
+        }
+        else
+        {
+            self.setDataAfterInAppPurchaseTransaction(isPurchased: false)
+        }
+        
+        // Fetch IAP Products available
+        fetchAvailableProducts()
+    }
+    
+    //FETCH AVAILABLE IAP PRODUCTS
+    func fetchAvailableProducts()  {
+        
+        // Put here your IAP Products ID's
+        let productIdentifiers = NSSet(objects:DONATE_PRODUCT_ID)
+        
+        productsRequest = SKProductsRequest(productIdentifiers: productIdentifiers as! Set<String>)
+        productsRequest.delegate = self
+        productsRequest.start() //start an SKProductsRequest based on identifier, in order for the app to display the info about the IAP products (description and price), which will be processed by below delegate method(productsRequest:didReceive)
+    }
+    
+    //REQUEST IAP PRODUCTS
+    func productsRequest (_ request:SKProductsRequest, didReceive response:SKProductsResponse) {
+        if (response.products.count > 0) {
+            iapProducts = response.products
+            
+            let firstProduct = response.products[0] as SKProduct
+            
+            // Get its price from iTunes Connect
+            let numberFormatter = NumberFormatter()
+            numberFormatter.formatterBehavior = .behavior10_4
+            numberFormatter.numberStyle = .currency
+            numberFormatter.locale = firstProduct.priceLocale
+            
+            // Get its price from iTunes Connect
+            numberFormatter.locale = firstProduct.priceLocale
+            let price2Str = numberFormatter.string(from: firstProduct.price)
+            
+            // Show its description
+            print(firstProduct.localizedDescription + "for just \(price2Str!)")
+//            nonConsumableLabel.text = secondProd.localizedDescription + "\nfor just \(price2Str!)"
+            // ------------------------------------
+        }
+    }
+    
+    private func setDataAfterInAppPurchaseTransaction(isPurchased : Bool){
+        if(isPurchased == true){
+            nonConsumablePurchaseMade = true
+            UserDefaults.standard.set(nonConsumablePurchaseMade, forKey: KKEY_PURCHASE_MADE)
+            btnDonate.title = "Restore ₹10"
+            btnDonate.tag = KRestoreButtonTag
+        }else{
+            nonConsumablePurchaseMade = false
+            UserDefaults.standard.set(nonConsumablePurchaseMade, forKey: KKEY_PURCHASE_MADE)
+            btnDonate.title = "Donate ₹10"
+            btnDonate.tag = KDonateButtonTag
+        }
+        
+    }
+    
+    //Store kit delegate methods
+    func paymentQueueRestoreCompletedTransactionsFinished(_ queue: SKPaymentQueue) {
+        // Restore your purchase locally (needed only for Non-Consumable IAP)
+        self.setDataAfterInAppPurchaseTransaction(isPurchased: false)
+        
+        UIAlertView(title: KALERT_TITLE,
+                    message: "You've successfully restored your purchase!",
+                    delegate: nil, cancelButtonTitle: "OK").show()
+    }
+    
+    // MARK:- IAP PAYMENT QUEUE
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+        for transaction:AnyObject in transactions {
+            if let trans = transaction as? SKPaymentTransaction {
+                switch trans.transactionState {
+                    
+                case .purchased:
+                    SKPaymentQueue.default().finishTransaction(transaction as! SKPaymentTransaction)
+                    
+                     if productID == DONATE_PRODUCT_ID
+                     {                        
+                        // Save your purchase locally (needed only for Non-Consumable IAP)
+                        self.setDataAfterInAppPurchaseTransaction(isPurchased: true)
+                        
+                        UIAlertView(title: KALERT_TITLE,
+                                    message: "You've successfully donate me ₹10! Thank you",
+                                    delegate: nil,
+                                    cancelButtonTitle: "OK").show()
+                    }
+                    
+                    break
+                    
+                case .failed:
+                    SKPaymentQueue.default().finishTransaction(transaction as! SKPaymentTransaction)
+                    Utility.showAlertMessage(title: "Not able to donate. Retry please.", subTitle: "", messageType: .error)
+
+                    break
+                case .restored:
+                    SKPaymentQueue.default().finishTransaction(transaction as! SKPaymentTransaction)
+                    break
+                    
+                default: break
+                }}}
     }
 }
 
